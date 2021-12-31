c89f6873e14727fa459f7ab27bb6ceb718c73692
LautaroC <lautarocutri@gmail.com>
2021-12-31 19:38:37 -0300

    Update

--- backend/tests/integration.int.test.ts
+++ backend/tests/integration.int.test.ts
@@ -2,43 +2,46 @@ import Ganache from "ganache-core";
 import { unwrap } from "../src/util";
 import { execSync } from "child_process";
 import * as path from "path";
-// import { test, expect, jest, beforeEach } from "@jest/globals";
+import { test, expect, jest, beforeEach } from "@jest/globals";
 
 
-const src = path.join("tests", "ganacheDB");
-const dist = path.join("tests", "copy_ganacheDB");
-// copy blockchain test env to prevent modifications
-console.warn("Copying DB")
-execSync(`rm -rf ${dist}`);
-execSync(`mkdir ${dist}`);
-execSync(`cp -r ${src}/* ${dist}`);
 
-const server = Ganache.server({
-  db_path: dist,
-  mnemonic: "polar velvet stereo oval echo senior cause cruel tube hobby exact angry",
-  network_id: 5777,
-  ws: false
-});
-server.on("listening", () => {
-  beginTest();
-})
-server.on('error', (e: { code: string }) => {
-  if (e.code === 'EADDRINUSE') {
-    console.warn('Address in use, retrying...');
+
+test("Integration", (done) => {
+  jest.setTimeout(30000);
+  let src = path.join("tests", "ganacheDB");
+  let dist = path.join("tests", "copy_ganacheDB");
+  // copy blockchain test env to prevent modifications
+  console.log("Copying DB")
+  execSync(`rm -rf ${dist}`);
+  execSync(`mkdir ${dist}`);
+  execSync(`cp -r ${src}/* ${dist}`);
+  
+  let server = Ganache.server({
+    db_path: dist,
+    mnemonic: "polar velvet stereo oval echo senior cause cruel tube hobby exact angry",
+    network_id: 5777,
+    ws: false
+  });
+  server.on("listening", () => {
+    beginTest();
+  })
+  server.on('error', (e: { code: string }) => {
+    if (e.code === 'EADDRINUSE') {
+      console.log('Address in use, retrying...');
+      server.close();
+    }
+  });
+  
+  console.log("Starting server")
+  server.listen(unwrap(process.env.GANACHE_PORT, "Ganache port not specified"));
+  
+  
+  let beginTest = () => {
+    console.log("Done")
     server.close();
+    done();
   }
-});
-
-console.warn("Starting server")
-server.listen(unwrap(process.env.GANACHE_PORT, "Ganache port not specified"));
 
 
-const beginTest = () => {
-  console.warn("Done")
-  server.close();
-}
-
-// test("Integration", async () => {
-
-
-// })
+})
